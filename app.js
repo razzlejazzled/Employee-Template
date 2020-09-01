bff04af4a1e7ccb8be41a900173b866cce1503a4
RazzleJazzled <robertjgassner@gmail.com>
2020-08-31 21:02:49 -0700

    exports

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const prompts = require("./lib/prompts")
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -10,6 +10,13 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+const {
+    managerPrompt,
+    addEmployee,
+    engineerPrompt,
+    internPrompt
+} = require("./lib/prompts")
 
 
 // Write code to use inquirer to gather information about the development team members,
